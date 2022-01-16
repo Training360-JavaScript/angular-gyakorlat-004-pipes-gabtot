f5a8c9e9614d37c4a245e787d9d755a7fbb44397
gabtot <gabor.toth.de@digikabel.hu>
2022-01-16 16:22:54 +0100

    kész

--- src/app/pipe/sorter.pipe.ts
+++ src/app/pipe/sorter.pipe.ts
@@ -11,16 +11,17 @@ export class SorterPipe implements PipeTransform {
    * @param key {string} - az objektumkulcs, amely alapján rendez
    * @returns {any[]} - a kulcs alapján rendezett tömb
    */
-  transform(value: any[], key: string): any[] {
+  transform(value: [], key: string): any[] {
     // A KÖVETKEZŐ SORT TÁVOLÍTSD EL!!!
-    return value;
 
     /**
      * FELADAT!
      * Ellenőrzés: ha a value nem tömb vagy nincs megadva a key,
      * térj vissza a value változóval.
      */
-
+    if ( !Array.isArray(value) || !key ) {
+      return value;
+    }
 
 
     /**
@@ -32,8 +33,15 @@ export class SorterPipe implements PipeTransform {
      * 3. Térj vissza a két string localeCompare metódus által visszaadott
      *  összehasonlításának az eredményével.
      */
-
-
+    return value.sort( ( a : any, b : any ) : number =>
+      (typeof a[key] === 'number' && typeof b[key] === 'number')
+        ?
+      a[key] - b[key]
+        :
+      [ a[key], b[key] ]
+        .map( s => s.toString().toLowerCase() )
+        .reduce( (a: string, b:string) : number => a.localeCompare(b) )
+    );
   }
 
 }
